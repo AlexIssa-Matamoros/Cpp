a92ad623c66b41c64b37e622438ada9484cc2c22
Alex Matamoros <isai.mat43@gmail.com>
2021-02-06 21:40:37 -0600

    Agregando Ejercicio1

---
+++ Ejercicios/Ejercicio1/Apellido.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string apodo;
+    cout << "ingrese el apodo: ";
+    cin >> apodo;
+
+    cout << endl;
+    cout << "Hola mi apodo es: " << apodo;
+    cout << endl;
+    
+    return 0;
+}
